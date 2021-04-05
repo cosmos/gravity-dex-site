2316d37caa9b6157aa280db12abde44a70a55b39
Cyrus Goh <hello@lovincyrus.com>
2021-04-05 10:27:40 -0700

    nuxt template init

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+export default {
+  /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: 'static',
+  ssr: true,
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [{ src: '~/assets/styles/main.styl', lang: 'stylus' }],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: ['~/plugins/vue-scrollto.client.js'],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://github.com/juliomrqz/nuxt-optimized-images
+    '@aceforth/nuxt-optimized-images',
+    // https://github.com/nuxt-community/style-resources-module
+    '@nuxtjs/style-resources',
+  ],
+  optimizedImages: {
+    optimizeImages: true,
+  },
+  styleResources: {
+    stylus: [
+      '~/assets/styles/abstracts/_*.styl',
+      '~/node_modules/rfs/stylus.styl',
+    ],
+  },
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [],
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    // https://nuxtjs.org/faq/postcss-plugins/#recommended-method
+    postcss: {
+      plugins: {
+        'postcss-font-variant': {},
+      },
+      preset: {
+        // To change the postcss-preset-env settings
+        autoprefixer: {},
+      },
+    },
+  },
+}
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <div class="section-hero">
+      <div class="tm-section tm-container tm-wrapper tm-grid-base">
+        <div class="text">
+          <h1>Hello world!</h1>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus" scoped>
+.section-hero .text
+  grid-column 1/-1
+  text-align center
+
+@media $breakpoint-xl
+  .section-hero .text
+    grid-column 2/span 10
+</style>
